abe5d96e686ecbf72adfaa3f0f35d56df8cd87f7
Henrik Bengtsson <hb@aroma-project.org>
2012-05-17 05:03:21 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.batch@784 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/jobs-mandelbrot/src/onFinally.R
+++ inst/jobs-mandelbrot/src/onFinally.R
@@ -1,7 +1,7 @@
 ####################################################################
 # There is a risk, although small, that the job is interrupted
 # or that an error occurs while creating the image. Regardless of
-# if the job was successful, and interrupts or an error occured, the
+# if the job was successful, an interrupt or an error occured, the
 # onFinally() function will always be called.
 ####################################################################
 onFinally <- function(...) {
--- inst/jobs-stub/src/onFinally.R
+++ inst/jobs-stub/src/onFinally.R
@@ -1,10 +1,9 @@
 ####################################################################
 # There is a risk, although small, that the job is interrupted
 # or that an error occurs while running. Regardless of if the job
-# was successful, and interrupts or an error occured, the
+# was successful, an interrupt or an error occured, the
 # onFinally() function will always be called.
 ####################################################################
 onFinally <- function(...) {
   # Clean up, e.g. close open devices, connections etc.
-  graphics.off();
 }
